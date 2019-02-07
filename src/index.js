5fa2a3a66085b34b6f523dabffab81b540e83b27
Bartomiej Dybowski <b.dybowski@gmail.com>
2019-02-07 17:37:43 +0100

    missing import added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import events from './data/events.json';
+
 ReactDOM.render(
   <p>Your app goes here...</p>,
   document.getElementById('root')
