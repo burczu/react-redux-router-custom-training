b1021f557017f6186d47f99ff74aa719a7fbe0d5
Bartomiej Dybowski <b.dybowski@gmail.com>
2019-02-09 19:32:15 +0100

    validation messages added

--- src/EventAdd.js
+++ src/EventAdd.js
@@ -2,28 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Text } from 'informed';
 
+const validateRequired =
+    value => !value || value.length === 0 ? 'Pole jest wymagane' : undefined;
+
 const EventAdd = (props) => {
   const { onFormSubmit, getForm } = props;
 
   return (
     <Form onSubmit={onFormSubmit} getApi={getForm}>
-      <label htmlFor="name">
-        Nazwa:
-        <Text id="name" field="name" />
-      </label>
-      <label htmlFor="place">
-        Miejsce:
-        <Text id="place" field="place" />
-      </label>
-      <label htmlFor="date">
-        Data:
-        <Text id="date" field="date" />
-      </label>
-      <label htmlFor="time">
-        Godzina:
-        <Text id="time" field="time" />
-      </label>
-      <button type="submit">Zapisz</button>
+      {({ formState }) => (
+        <>
+          <label htmlFor="name">
+            Nazwa:
+            <Text id="name" field="name" validate={validateRequired} />
+            {formState.errors.name && <span>{formState.errors.name}</span>}
+          </label>
+          <label htmlFor="place">
+            Miejsce:
+            <Text id="place" field="place" validate={validateRequired} />
+            {formState.errors.name && <span>{formState.errors.name}</span>}
+          </label>
+          <label htmlFor="date">
+            Data:
+            <Text id="date" field="date" validate={validateRequired} />
+            {formState.errors.name && <span>{formState.errors.name}</span>}
+          </label>
+          <label htmlFor="time">
+            Godzina:
+            <Text id="time" field="time" validate={validateRequired} />
+            {formState.errors.name && <span>{formState.errors.name}</span>}
+          </label>
+          <button type="submit">Zapisz</button>
+        </>
+      )}
     </Form>
   );
 };
