8403598e904a9fd29013d6a0958c53e847eba6f7
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-04-10 17:23:58 +0200

    comment added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import events from './data/events.json';
 ReactDOM.render(
   <ul>
     {events.map(item => {
+      // jeśli zwracamy wiele linii jsx, powinniśmy owijać je w nawiasy
       return (
         <li key={item.id}>
           <strong>{item.name}</strong><br />
