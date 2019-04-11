4af7017abdf59ef0a68c772f89aba507c7bfe604
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-04-11 18:02:04 +0200

    comments added

--- src/Events.js
+++ src/Events.js
@@ -6,18 +6,32 @@ class Events extends React.Component {
     events: PropTypes.array.isRequired,
   };
 
+  // początkowy stan komponentu
+  // definiujemy jako właściwość klasy
   state = { events: [] };
 
+  // metoda cyklu życia komponentu
+  // uruchamiana jeden raz, zaraz po
+  // pierwszym wyrenderowaniu komponentu
   componentDidMount() {
+    // tutaj bierzemy events z "propsów" czyli dostajemy z zewnątrz
     const { events } = this.props;
+
+    // tylko wywołanie metody `setState` powoduje zmianę stanu
+    // zmiana stanu powoduje ponowne renderowanie komponentu
+    // już z nowymi wartościami stanu
     this.setState({
-      events,
+      events, // składnia ES6 - skrót od events: events
     });
   }
 
   render() {
+    // przy pierwszym renderowaniu `events` to pusta tablica (patrz stan początkowy)
+    // następnie w `componentDidMount` stan się zmienia i następuje drugie renderowanie
     const { events } = this.state;
 
+    // przy pierwszym renderze zwracany jest `<ul></ul>`
+    // przy kolejnym już `<ul><li></li>...</ul>`
     return (
       <ul>
         {events.map(item => {
