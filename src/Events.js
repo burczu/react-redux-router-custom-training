a403b727d48944c9e511d32d5bd1ac32daf71a1b
Bartek Dybowski <bartlomiej.dybowski@goingapp.pl>
2019-04-11 17:43:12 +0200

    comments added

--- src/Events.js
+++ src/Events.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// komponent to funkcja, której nazwa pisana jest z wielkiej litery
 const Events = (props) => {
+  // obiekt props zawiera wartości parametrów przekazanych
+  // przy wywołaniu komponentu w komponencie rodzicu (sprawdź w `index.js`)
   const { events } = props;
 
   return (
@@ -25,8 +28,13 @@ const Events = (props) => {
   );
 };
 
+// pamiętaj o propTypes - w ten sposób skontrolujesz
+// czy dane przekazane do komponentu mają odpowiedni typ!
 Events.propTypes = {
   events: PropTypes.array.isRequired,
 };
 
+// export z użyciem "default"
+// tym razem import wyglądać będzie tak (brak {}):
+// `import Events from 'Events'` (sprawdź w `index.js`)
 export default Events;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import events from './data/events.json';
+
+// import bez {}, ponieważ Events exportuje komponent jako "default"
 import Events from './Events';
 
 ReactDOM.render(
+  // komponentów używamy tak jakby były "tagami" html
+  // to co przekażemy do atrybutu `events` będzie
+  // dostępne w obiekcie "props" funkcji `Events`
   <Events events={events} />,
   document.getElementById('root')
 );
